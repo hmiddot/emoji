c40814445fa63fc89fb3947202fb907786e422fe
HRA <122253551+hmiddot@users.noreply.github.com>
2023-08-22 17:15:19 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-        function installEmojis(element = "*", size = 1.4, marginTop = 0.3) {
-            if (element[0] === "#") {
-                element = document.querySelector(element);
-            } else {
-                element = document.querySelectorAll(element);
-            }
-            for (let i = 0; i < element.length; i++) {
-                let elm = element[i];
-                const emojiRegex = /([\uD800-\uDBFF][\uDC00-\uDFFF])/;
-                const textNodes = document.createTreeWalker(
-                    elm,
-                    NodeFilter.SHOW_TEXT,
-                    null,
-                    false
-                );
-                while (textNodes.nextNode()) {
-                    const node = textNodes.currentNode;
-                    let matches;
-                    while ((matches = node.nodeValue.match(emojiRegex))) {
-                        const emojiElement = document.createElement("img");
-                        emojiElement.alt = matches[0];
-                        emojiElement.src = `https://raw.githubusercontent.com/hmiddot/emoji/main/files/${matches[0]}.png`;
-                        emojiElement.style = 'width:' + size + 'em;display:inline-block;margin-top:' + marginTop + 'em;margin-bottom:-' + marginTop + 'em';
-                        const emojiIndex = node.nodeValue.indexOf(matches[0]);
-                        const preMatchText = node.nodeValue.slice(0, emojiIndex);
-                        const postMatchText = node.nodeValue.slice(emojiIndex + 2);
-                        const preTextNode = document.createTextNode(preMatchText);
-                        node.nodeValue = postMatchText;
-                        node.parentNode.insertBefore(emojiElement, node);
-                        node.parentNode.insertBefore(preTextNode, node);
-                        textNodes.currentNode = preTextNode;
-                    }
-                }
+function installEmojis(element = "*", size = 1.4, marginTop = 0.3) {
+    if (element[0] === "#") {
+        element = document.querySelector(element);
+    } else {
+        element = document.querySelectorAll(element);
+    }
+    for (let i = 0; i < element.length; i++) {
+        let elm = element[i];
+        const emojiRegex = /([\uD800-\uDBFF][\uDC00-\uDFFF])/;
+        const textNodes = document.createTreeWalker(elm, NodeFilter.SHOW_TEXT, null, false);
+        while (textNodes.nextNode()) {
+            const node = textNodes.currentNode;
+            let matches;
+            while ((matches = node.nodeValue.match(emojiRegex))) {
+                const emojiElement = document.createElement("img");
+                emojiElement.alt = matches[0];
+                emojiElement.draggable = false;
+                emojiElement.src = `https://raw.githubusercontent.com/hmiddot/emoji/main/files/${matches[0]}.png`;
+                emojiElement.style = 'width:' + size + 'em;display:inline-block;margin-left:.1em;margin-right:.1em;margin-top:' + marginTop + 'em;margin-bottom:-' + marginTop + 'em';
+                const emojiIndex = node.nodeValue.indexOf(matches[0]);
+                const preMatchText = node.nodeValue.slice(0, emojiIndex);
+                const postMatchText = node.nodeValue.slice(emojiIndex + 2);
+                const preTextNode = document.createTextNode(preMatchText);
+                node.nodeValue = postMatchText;
+                node.parentNode.insertBefore(emojiElement, node);
+                node.parentNode.insertBefore(preTextNode, node);
+                textNodes.currentNode = preTextNode;
             }
         }
+    }
+}
+
+installEmojis('body',1.4,.07 );
